ef2389ea190a70687ce8fa7ea9a1e168fb069b86
Matthew Behrens <mattbehrens96@gmail.com>
2017-02-02 19:00:52 -0600

    config files

--- config/application.rb
+++ config/application.rb
@@ -10,16 +10,15 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+#require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
-
 module MaineDental
   class Application < Rails::Application
+  	config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
