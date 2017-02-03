bfbe8a1d113ffe930fca9fae57493f0d45c98176
Matthew Behrens <mattbehrens96@gmail.com>
2017-02-02 19:10:58 -0600

    fix gemfile?

--- config/application.rb
+++ config/application.rb
@@ -18,9 +18,9 @@ Bundler.require(*Rails.groups)
 
 module MaineDental
   class Application < Rails::Application
-  	config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
   end
 end
